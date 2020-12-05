bf141080c71e2a782480d9be92cfbb035c34192b
Devmack <dominik.starzyk@codecool.com>
2020-12-05 16:31:21 +0100

    Resolve conflict

--- gittie.py
+++ gittie.py
@@ -1,10 +1,8 @@
 def introduction():
     print("hello i'm gittie!")
 
-<<<<<<< HEAD
 def add():
     print('add')
-=======
+    
 def joke():
     print("HEHEHE")
->>>>>>> 593869a4c2043eca2d728020331e30d8ed041caf
